2018a5e4991a04914282236011c45edb522c8d5f
succatash <mstein1284@gmail.com>
2023-07-01 18:29:31 -0400

    assed a bunch of other features

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import './App.css';
+import {puppyList} from './data';
+import {useState} from 'react';
 
 function App() {
-	return <></>;
+	const [puppies, setPuppies] = useState(puppyList);
+	const [featPupId, setPuppyId] = useState(null);
+
+	const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+	return (
+		<>
+			{puppies.map((puppy) => {
+				return (
+					<p
+						key={puppy.id}
+						onClick={() => {
+							setPuppyId(puppy.id);
+						}}
+					>
+						{puppy.name}
+					</p>
+				);
+			})}
+
+			{featPupId && (
+				<div>
+					<h2>{featuredPup.name}</h2>
+					<ul>
+						<li className='list'>Age: {featuredPup.age}</li>
+						<li className='list'>Email: {featuredPup.email}</li>
+					</ul>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default App;
